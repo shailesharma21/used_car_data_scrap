2d484cd8b63c7a365659fe4a6a2182234440364e
shailesharma21 <shailesh.sharma@tigeranalytics.com>
2022-05-19 13:38:39 +0530

    car data scrap using request

--- carroya.py
+++ carroya.py
@@ -5,9 +5,9 @@ from random import randint
 from time import sleep
 from datetime import datetime
 
+
 print("start: ", datetime.now())
 start_url = "https://carroya-pro-portal-api.avaldigitallabs.com/find-vehicle-detail/"
-car_urls = pd.read_csv("car_urls_1805.csv")
 
 header = {
     "accept": "application/json, text/plain, */*",
@@ -26,41 +26,64 @@ header = {
 }
 
 
-def scraper(url):
-    sleep(randint(3, 6))
-    car_id = car_urls["URL"][url].split("/")[-1]
+def scrap_car_id(page_num):
+    url = "https://carroya-pro-portal-api.avaldigitallabs.com/find-filters"
+    payload = json.dumps(
+        {"seoArray": [], "params": {"page": str(page_num), "status": "usado"}}
+    )
+    headers = {"Content-Type": "text/plain"}
+    response = requests.request("POST", url, headers=headers, data=payload)
+    res_data = response.json()
+    car_ids = [x["id"] for x in res_data["results"]["superHighlights"]]
+    return car_ids
+
+
+def scraper(car_id_list):
+    car_data_list = []
+    for car_id in car_id_list:
+        sleep(randint(3, 8))
+        try:
+            url_response = requests.get(start_url + str(car_id), headers=header)
+            car_data = json.loads(url_response.text)
+            car_detail = {
+                "Car id": car_data["data"]["id"],
+                "Brand": car_data["data"]["title"],
+                "Condition": car_data["data"]["status"],
+                "Version": car_data["data"]["version"],
+                "Kilometers": car_data["data"]["kilometers"],
+                "Year": car_data["data"]["year"],
+                "City": car_data["data"]["city"],
+                "Color": car_data["data"]["color"],
+                "Cylindrical": car_data["data"]["cylindrical"],
+                "Box type": car_data["data"]["box"],
+                "Fuel": car_data["data"]["fuel"],
+                "Accessories": car_data["data"]["accessories"],
+                "Seller comment": car_data["data"]["comments"],
+                "Main features": car_data["data"]["main-features"],
+                "Technical details": car_data["data"]["technical_sheet"],
+            }
+            car_data_list.append(car_detail)
+        except:
+            car_detail = {"Car id": str(car_id)}
+            car_data_list.append(car_detail)
+    return car_data_list
+
+
+all_car_data = []
+# Using a fixed no of pages for testing in for loop
+for page in range(1, 14):
+    sleep(randint(3, 8))
     try:
-        url_response = requests.get(start_url + car_id, headers=header)
-        car_data = json.loads(url_response.text)
-        car_detail = {
-            "Car id": car_data["data"]["id"],
-            "Brand": car_data["data"]["title"],
-            "Condition": car_data["data"]["status"],
-            "Version": car_data["data"]["version"],
-            "Kilometers": car_data["data"]["kilometers"],
-            "Year": car_data["data"]["year"],
-            "City": car_data["data"]["city"],
-            "Color": car_data["data"]["color"],
-            "Cylindrical": car_data["data"]["cylindrical"],
-            "Box type": car_data["data"]["box"],
-            "Fuel": car_data["data"]["fuel"],
-            "Accessories": car_data["data"]["accessories"],
-            "Seller comment": car_data["data"]["comments"],
-            "Main features": car_data["data"]["main-features"],
-            "Technical details": car_data["data"]["technical_sheet"],
-        }
-        return car_detail
+        car_ids = scrap_car_id(page)
+        if car_ids:
+            out_car_data = scraper(car_ids)
+            all_car_data.extend(out_car_data)
+        else:
+            continue
     except:
-        car_detail = {"Car id": car_id}
-        return car_detail
-
-
-out_list = []
-# checking for 200 urls
-for url in range(0, 200):
-    out_list.append(scraper(url))
+        pass
 
 # Create dataframe from all cars data
-car_df = pd.DataFrame(out_list)
-car_df.to_csv("scraped_car_info_200urls.csv")
+car_df = pd.DataFrame(all_car_data)
+car_df.to_csv("car_data_for_208.csv")
 print("end: ", datetime.now())
